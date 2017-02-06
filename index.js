2327e1d2fdfbafdaaccb96c44eaad652a5919f00
André Murilo <andremurilolopeselias@gmail.com>
2017-02-06 19:43:41 -0200

    adicionando habilidades

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ var app = express();
 var users = [
   {
     "nome" : "Gustavo",
-    "idade" : 21
+    "idade" : 21,
+    "habilidades" : [{"nome":"Design"},{"":"Javascript"}]
   },
   {
     "nome" : "Biris",
-    "idade" : 22
+    "idade" : 22,
+    "habilidades" : [{"nome":"Javascript"}]
   }
 ]
 
